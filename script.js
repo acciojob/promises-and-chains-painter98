26ef49ca3226040dbd051b1d6976cbb00d41d275
painter98 <120320713+painter98@users.noreply.github.com>
2023-04-04 14:28:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function execute(){
+	let age = document.getElementById("age").value;
+	let name = document.getElementById("name").value;
+	
+	let promise = new Promise((resolve,reject) => {
+		setTimeout(function(){
+			if(age>=18) resolve(`Welcome, ${name}. You can vote.`);
+			else reject(`Oh sorry ${name}. You aren't old enough.`);
+		},4000);
+	})
+	promise.then(data => {console.log(data)}).catch(data => {console.log(data)});
+}
